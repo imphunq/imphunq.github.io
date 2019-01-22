c3c2dd2d4cfc1f61053f27dbe71f7d9c27d09705
unknown <quangphunguyen232@gmail.com>
2019-01-22 22:58:13 +0700

    xong

--- 1.js
+++ 1.js
@@ -1,4 +1,8 @@
 ﻿ $(function(){
- 
+ 	vitricuaedu = $('#khoiedu').offset().top;
+ 	// console.log(vitricuaedu);
+ 	$('.xuong,.kn').click(function(){
+ 		$('html,body').animate({scrollTop:vitricuaedu},800)
+ 	})
 })  
  
