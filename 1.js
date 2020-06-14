96bd85763d37060a68e124b907f3d4099d67406d
quangphu <nguyen.quang.phu@sun-asterisk.com>
2020-06-14 21:27:48 +0700

    change structure to vuejs

--- 1.js
+++
@@ -1,8 +0,0 @@
-﻿ $(function(){
- 	vitricuaedu = $('#khoiedu').offset().top;
- 	// console.log(vitricuaedu);
- 	$('.xuong,.kn').click(function(){
- 		$('html,body').animate({scrollTop:vitricuaedu},800)
- 	})
-})  
- 
